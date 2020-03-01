7826f8bbba46efcb234e556fe58501c9285e3fde
brayooh <brianndichu.bn@gmail.com>
2020-03-01 12:33:23 +0300

    $ git config user.name

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,2 +1,15 @@
 from django.conf import settings
 from . import views
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+
+
+urlpatterns=[
+    url(r'^$', views.posted_images, name='home'),
+    url(r'^search/', views.search_results,name='search_results'), 
+   
+]
+
+
