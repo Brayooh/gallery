1ba7b2f915f0d662c500d3de6297c06771f2f244
brayooh <brianndichu.bn@gmail.com>
2020-03-01 17:09:49 +0300

    creates view for searched results

--- gallery/views.py
+++ gallery/views.py
@@ -9,7 +9,23 @@ def posted_images(request):
 
     return render(request, 'index.html', {"pics":pics})
 
+# def add_image(request):
 
+
+#     return render(request, 'add_image.html')
+
+
+def search_results(request):
+    if 'pictures' in request.GET and request.GET["pictures"]:
+        searched_term = request.GET.get("pictures")
+        searched_pictures = image.search_by_category(searched_term)
+        message = f"{searched_term}"
+
+        return render(request, 'search.html', {"message": message, "pictures": searched_pictures})
+
+    else:
+        message = "You have not searched for any picture"
+        return render(request, 'search.html', {"message": message})
 
 
 # def copy_to_clipboard(request):
